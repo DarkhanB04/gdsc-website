81edadbcb11496613aab19e514405c71000842b6
Darkhan B <darkhanbekuzak13@gmail.com>
2023-01-19 18:10:34 +0600

    7 commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const workshopCarousel = document.querySelector(".workshop-list-carousel");
 const workshopList = document.querySelector(".workshop-list");
 const workshops = document.getElementsByClassName("workshop");
-const eclipses = document.getElementsByClassName("eclipses")
+const eclipses = document.getElementsByClassName("eclipses");
 
 if (window.matchMedia("(max-width: 768px)").matches) {
   workshopCarousel.classList.add("swiper", "workshopSwiper");
@@ -13,10 +13,7 @@ if (window.matchMedia("(max-width: 768px)").matches) {
 
   /* the viewport is at least 400 pixels wide */
 }
-if(window.matchMedia("(max-width:1200px").matches)
-{
-  eclipses.classList.add("active")
-}
+
 var swiper1 = new Swiper(".workshopSwiper", {
   slidesPerView: 2,
   grabCursor: true,
@@ -26,14 +23,17 @@ var swiper1 = new Swiper(".workshopSwiper", {
     prevEl: ".workshop-pagination-button-prev",
   },
   breakpoints: {
-    0:{
+    0: {
       slidesPerView: 2,
     },
-    768:{
+    768: {
       slidesPerView: 2,
-    }
+    },
   },
 });
+if (window.matchMedia("(max-width:1200px").matches) {
+  eclipses.classList.add("active");
+}
 var swiper2 = new Swiper(".mySwiper", {
   slidesPerView: 4,
   grabCursor: true,
@@ -42,9 +42,9 @@ var swiper2 = new Swiper(".mySwiper", {
     nextEl: ".members-pagination-button-next",
     prevEl: ".members-pagination-button-prev",
   },
-  
+
   breakpoints: {
-    0:{
+    0: {
       slidesPerView: 2,
     },
     768: {
@@ -54,42 +54,40 @@ var swiper2 = new Swiper(".mySwiper", {
 });
 
 document.addEventListener("mousemove", (e) => {
-  console.log(e)
+  console.log(e);
   const mouseX = e.clientX;
   const mouseY = e.clientY;
-  const anchor = document.querySelector(".google-man-img")
+  const anchor = document.querySelector(".google-man-img");
   const rekt = anchor.getBoundingClientRect();
   const anchorX = rekt.left + rekt.width / 2;
   const anchorY = rekt.top + rekt.height / 2;
-  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
-  const leftEye = document.querySelector(".google-man-lefteye")
-  leftEye.style.transform = `rotate(${angleDeg+90}deg)`;
-  const righEye = document.querySelector(".google-man-righteye")
-  righEye.style.transform = `rotate(${angleDeg+90}deg)`;
-})
-function angle(cx, cy, ex, ey){
-  const dy = ey -cy;
-  const dx = ex-cx;
+  const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
+  const leftEye = document.querySelector(".google-man-lefteye");
+  leftEye.style.transform = `rotate(${angleDeg + 90}deg)`;
+  const righEye = document.querySelector(".google-man-righteye");
+  righEye.style.transform = `rotate(${angleDeg + 90}deg)`;
+});
+function angle(cx, cy, ex, ey) {
+  const dy = ey - cy;
+  const dx = ex - cx;
   const rad = Math.atan2(dy, dx);
-  const deg = rad *180/Math.PI;
+  const deg = (rad * 180) / Math.PI;
   return deg;
 }
-const googleMan = document.querySelector(".google-man")
-const googleManImg =document.querySelector(".google-man-img")
-googleManImg.addEventListener("mouseover", ()=> {
-  googleMan.style.transform = "translateY(80px)"
-  googleMan.style.transition ="0.7s"
-})
+const googleMan = document.querySelector(".google-man");
+const googleManImg = document.querySelector(".google-man-img");
+googleManImg.addEventListener("mouseover", () => {
+  googleMan.style.transform = "translateY(80px)";
+  googleMan.style.transition = "0.7s";
+});
 googleManImg.addEventListener("mouseout", () => {
-  googleMan.style.transform = "translateY(0px)"
-  googleMan.style.transition ="1s"
-
-})
+  googleMan.style.transform = "translateY(0px)";
+  googleMan.style.transition = "1s";
+});
 
 let list = document.querySelectorAll(".menu-item");
-function activeLink(){
-  list.forEach((item)=>
-    item.classList.remove('active'))
-    this.classList.add('active')
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
 }
-list.forEach((item) => item.addEventListener('click', activeLink))
+list.forEach((item) => item.addEventListener("click", activeLink));
